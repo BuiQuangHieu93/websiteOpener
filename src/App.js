8d43489554a9ea9b7c7a90c610555c2e86f7b302
BuiQuangHieu93 <zzzqhieu@gmail.com>
2023-12-28 21:37:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Function to open the website
+    const openWebsite = () => {
+      window.open(
+        "https://login.wifidwn.vn/login?dst=&username=T-48%3A89%3AE7%3AE4%3A18%3AB2",
+        "_blank"
+      );
+    };
+
+    // Set interval to open the website every 15 minutes (900000 milliseconds)
+    const intervalId = setInterval(openWebsite, 900000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Website Opener</h1>
+      <p>This website will open https://www.youtube.com/ every 15 minutes.</p>
     </div>
   );
 }
